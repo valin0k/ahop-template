dc15228b8f26752b4ef25e35ff1bff65e9e0560e
Pavel <serpork@ya.ru>
2020-05-22 22:33:09 +0700

    add icons and update topbar

--- main/components/TopBar/index.js
+++ main/components/TopBar/index.js
@@ -1,22 +1,24 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Image } from 'react-native'
 import { observer } from 'startupjs'
-import { Span } from '@startupjs/ui'
 import { Text } from 'components'
+import { BASE_URL } from 'clientHelpers'
 import './index.styl'
 
 const MENU_ITEMS = ['HOME', 'ABOUT', 'SHOP', 'PAGES', 'BLOG', 'CONTACT']
+const ICONS = ['/search.png', '/bag.png', '/menu.png']
 
 export default observer(function TopBar () {
   return pug`
     View.root
       View.logo
-        Text(title) Neha.
+        Image.logo(source={uri: BASE_URL + '/logo.png'})
       View.menu
         each item in MENU_ITEMS
           View.menuItem
-            Text(subtitle)=item
+            Text(bold)=item
       View.cart
-      
+        each icon, i in ICONS
+          Image.icon(source={uri: BASE_URL + icon} styleName={first: !i})
   `
 })
